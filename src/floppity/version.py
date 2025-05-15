28a1d93036ed5ed5f2a491c8721ad815b8e2f0c7
Francisco Ardevol Martinez <68873290+franciscoardevolmartinez@users.noreply.github.com>
2025-05-15 16:01:40 -0400

    versioning

--- src/floppity/version.py
+++ src/floppity/version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.0.2.dev21+ge78e06a.d20250515'
-__version_tuple__ = version_tuple = (0, 0, 2, 'dev21', 'ge78e06a.d20250515')
+__version__ = version = '0.0.4.dev0+g44cc3be.d20250515'
+__version_tuple__ = version_tuple = (0, 0, 4, 'dev0', 'g44cc3be.d20250515')
